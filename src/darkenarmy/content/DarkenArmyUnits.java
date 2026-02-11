873f78175b3ab324eb0b34d64db9a6e680a50712
Malthael972 <frankvaliquette123@gmail.com>
2026-02-10 23:38:42 -0600

    Update DarkenArmyUnits.java

--- src/darkenarmy/content/DarkenArmyUnits.java
+++ src/darkenarmy/content/DarkenArmyUnits.java
@@ -1,1 +1,85 @@
-package darkenarmy.content.;
+package darkenarmy.content;
+
+import arc.graphics.*;
+import arc.graphics.g2d.*;
+import arc.math.*;
+import arc.math.geom.*;
+import arc.struct.*;
+import arc.util.*;
+import mindustry.ai.*;
+import mindustry.ai.types.*;
+import mindustry.entities.*;
+import mindustry.entities.abilities.*;
+import mindustry.entities.bullet.*;
+import mindustry.entities.effect.*;
+import mindustry.entities.part.*;
+import mindustry.entities.pattern.*;
+import mindustry.content.Fx;
+import mindustry.gen.*;
+import mindustry.graphics.*;
+import mindustry.type.*;
+import mindustry.type.ammo.*;
+import mindustry.type.unit.*;
+import mindustry.type.weapons.*;
+import mindustry.type.Weapon;
+import mindustry.world.meta.*;
+import mindustry.content.StatusEffects;
+
+import static arc.graphics.g2d.Draw.*;
+import static arc.graphics.g2d.Lines.*;
+import static arc.math.Angles.*;
+import static mindustry.Vars.*;
+
+public class DarkArmyUnits {
+  public static UnitType darkStell;
+
+  public static void load(){
+    
+    darkStell = new TankUnitType("dark-stell"){{
+        constructor = UnitEntity::create;
+      
+            hitSize = 12f;
+            treadPullOffset = 3;
+            speed = 0.75f;
+            rotateSpeed = 3.5f;
+            health = 850;
+            armor = 6f;
+            itemCapacity = 0;
+            floorMultiplier = 0.95f;
+            treadRects = new Rect[]{new Rect(12 - 32f, 7 - 32f, 14, 51)};
+            researchCostMultiplier = 0f;
+            tankMoveVolume *= 0.32f;
+            tankMoveSound = Sounds.tankMoveSmall;
+
+            weapons.add(new Weapon("dark-stell-weapon"){{
+                shootSound = Sounds.shootStell;
+                layerOffset = 0.0001f;
+                reload = 50f;
+                shootY = 4.5f;
+                recoil = 1f;
+                rotate = true;
+                rotateSpeed = 2.2f;
+                mirror = false;
+                x = 0f;
+                y = -0.75f;
+                heatColor = Color.valueOf("f9350f");
+                cooldownTime = 30f;
+
+                bullet = new BasicBulletType(4f, 40){{
+                    sprite = "missile-large";
+                    smokeEffect = Fx.shootBigSmoke;
+                    shootEffect = Fx.shootBigColor;
+                    width = 5f;
+                    height = 7f;
+                    lifetime = 40f;
+                    hitSize = 4f;
+                    hitColor = backColor = trailColor = Color.valueOf("feb380");
+                    frontColor = Color.white;
+                    trailWidth = 1.7f;
+                    trailLength = 5;
+                    despawnEffect = hitEffect = Fx.hitBulletColor;
+                }};
+            }});
+        }};
+   }
+}

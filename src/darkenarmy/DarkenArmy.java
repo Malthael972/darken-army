7e139f7c6455153e6d1986fd9eea5826b9815cd8
Malthael972 <frankvaliquette123@gmail.com>
2026-02-10 23:45:27 -0600

    Update DarkenArmy.java

--- src/darkenarmy/DarkenArmy.java
+++ src/darkenarmy/DarkenArmy.java
@@ -2,10 +2,14 @@ package darkenarmy;
 
 import arc.*;
 import mindustry.mod.Mod;
+import darkenarmy.content.DarkenArmyUnits;
+import darkenarmy.content.DarkenArmyBlocks;
 
 public class DarkenArmy extends Mod {
     @Override
 public void loadContent() {
+    DarkenArmyUnits.load();
+    DarkenArmyBlocks.load();
   
 
  }

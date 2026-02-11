a58fde8686d0d1ae8ef882dd3388a19a77ee6eb0
Malthael972 <frankvaliquette123@gmail.com>
2026-02-11 00:09:40 -0600

    Create DarkenArmyTechTree.java

---
+++ src/darkenarmy/content/DarkenArmyTechTree.java
@@ -0,0 +1,28 @@
+package darkenarmy.content;
+
+import arc.struct.*;
+import mindustry.game.Objectives.*;
+import mindustry.type.*;
+import mindustry.content.*;
+import mindustry.ctype.*;
+
+import static mindustry.content.Blocks.*;
+import static mindustry.content.TechTree.*;
+import static darkarmy.content.DarkenArmyBlocks.*;
+import static darkarmy.content.DarkenArmyUnits.*;
+
+public class DarkenArmyTechTree {
+
+    public static void load() {
+      TechNode tankFabNode = tankFabricator.techNode;
+        if (tankFabNode != null) {
+            TechNode darkTankFabNode = new TechNode(tankFabNode, darkTankFabricator, 
+                darkTankFabricator.researchRequirements());
+            darkTankFabNode.objectives.add(new OnSector(SectorPresets.lake));
+            
+            // Add dark stell unit
+            TechNode darkStellNode = new TechNode(darkTankFabNode, darkStell, 
+                darkStell.researchRequirements());
+    
+    }
+}
